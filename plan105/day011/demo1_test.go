2871661a5011f39912364574e4be9f46a5ff8dfd
BinyGo <58040087@qq.com>
2022-04-27 08:41:27 +0800

    feat:plan105:day010-12

---
+++ plan105/day011/demo1_test.go
@@ -0,0 +1,7 @@
+package day011
+
+import "testing"
+
+func TestDemo1(t *testing.T) {
+	Demo1()
+}
---
+++ plan105/day012/demo2_test.go
@@ -0,0 +1,9 @@
+package day012
+
+import (
+	"testing"
+)
+
+func TestDemo1(t *testing.T) {
+	Demo1()
+}
