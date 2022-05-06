992aac9d7c81c025e2d5dd1c9dbc6e05e39394e3
BinyGo <58040087@qq.com>
2022-05-06 15:10:31 +0800

    feat:bcrypt包对password的简单示例

--- net/http/password/password.go
+++ net/http/password/password.go
@@ -35,3 +35,16 @@ func PassWordHashingHandler(w http.ResponseWriter, r *http.Request) {
 	cost := GetHashingCost([]byte(hash))
 	fmt.Fprintln(w, "Cost:      ", cost)
 }
+
+// func RegisterRoutes(r *mux.Router) {
+// 	...
+// 	indexRouter := r.PathPrefix("/index").Subrouter()
+// 	indexRouter.HandleFunc("/password_hashing", handler.PassWordHashingHandler)
+// 	...
+// }
+
+//curl -X GET http://localhost:8999/index/password_hashing
+// Password: secret
+// Hash:     $2a$14$Ael8nW7UF/En/iI7LGdyBuaIO8VREbL2CAShRN0EUQHqtmOHXh.XK
+// Match:    true
+// Cost:     14
