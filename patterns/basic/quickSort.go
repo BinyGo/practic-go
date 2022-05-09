190803cee831f2ff4772b3b16d8e16b29b3ec835
BinyGo <58040087@qq.com>
2022-05-09 11:03:02 +0800

    feat:Quick快速排序(原地,左右指针,双边循环),bench基准测试排序效率

---
+++ patterns/basic/quickSort.go
@@ -0,0 +1,68 @@
+package basic
+
+func QuickSort(arr []int) []int {
+	if len(arr) <= 1 {
+		return arr
+	}
+	splitData := arr[0]          //第一个数据
+	low := make([]int, 0)        //比我小的数据
+	hight := make([]int, 0)      //比我大的数据
+	mid := make([]int, 0)        //一样大的数据
+	mid = append(mid, splitData) //加入一个
+	for i := 1; i < len(arr); i++ {
+		if arr[i] < splitData {
+			low = append(low, arr[i])
+		} else if arr[i] > splitData {
+			hight = append(hight, arr[i])
+		} else {
+			mid = append(mid, arr[i])
+		}
+	}
+	low, hight = QuickSort(low), QuickSort(hight)
+	myArr := append(append(low, mid...), hight...)
+	return myArr
+}
+
+func QuickSortExec() {
+	arr := []int{1, 9, 10, 30, 2, 5, 45, 8, 63, 234, 12, 1, 9, 10, 30, 2, 5, 45, 8, 63, 234, 12}
+	QuickSort(arr)
+	//fmt.Println(QuickSort(arr))
+
+}
+
+func QuickExec() {
+	arr2 := []int{1, 9, 11, 30, 2, 5, 45, 8, 63, 235, 12, 1, 9, 11, 30, 2, 5, 45, 8, 63, 235, 12}
+	Quick(arr2, 0, len(arr2)-1) //原地排序,最优解法
+	//fmt.Println(arr2)
+}
+
+//原地排序,利用了左右指针.双边循环交换法,基准测试是QuickSort的10几倍,且不需额外内存,原地交换排序
+func Quick(arr []int, start, end int) {
+	if start >= end {
+		return
+	}
+	pivot := partition(arr, start, end)
+	Quick(arr, start, pivot-1)
+	Quick(arr, pivot+1, end)
+
+}
+
+func partition(arr []int, start, end int) int {
+	pivot := arr[start]
+	left := start
+	right := end
+	for left < right {
+		for left < right && arr[right] > pivot {
+			right -= 1
+		}
+		for left < right && arr[left] <= pivot {
+			left += 1
+		}
+		if left < right {
+			arr[left], arr[right] = arr[right], arr[left]
+		}
+	}
+	arr[start] = arr[left]
+	arr[left] = pivot
+	return left
+}
---
+++ patterns/basic/quickSort_test.go
@@ -0,0 +1,62 @@
+package basic
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestQuickSort(t *testing.T) {
+	type args struct {
+		arr []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{"one", args{arr: []int{1, 9, 10, 30, 2, 5, 45, 8, 63, 234, 12}}, []int{1, 2, 5, 8, 9, 10, 12, 30, 45, 63, 234}},
+		{"two", args{arr: []int{1, 9, 10, 30, 2, 5, 45, 8, 63, 234, 12}}, []int{1, 2, 5, 8, 9, 10, 12, 30, 45, 63, 234}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := QuickSort(tt.args.arr); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("QuickSort() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+func Test_Quick(t *testing.T) {
+	QuickSortExec()
+	QuickExec()
+}
+
+func BenchmarkQuickSort(b *testing.B) {
+
+	for i := 0; i < b.N; i++ {
+		QuickSortExec()
+	}
+}
+
+func BenchmarkQuick(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		QuickExec()
+	}
+}
+
+// go test -bench=".*" -benchmem -benchtime=10s -cpu=10  //-benchmem #输出内存分配统计  -benchtime=10s # 指定测试时间
+// goos: linux
+// goarch: amd64
+// pkg: github.com/practic-go/patterns/basic
+// cpu: 12th Gen Intel(R) Core(TM) i5-12600KF
+// BenchmarkQuickSort-16            1414204               847.9 ns/op          1216 B/op         50 allocs/op
+// BenchmarkQuick-16               23924130                48.88 ns/op            0 B/op          0 allocs/op
+// PASS
+// ok      github.com/practic-go/patterns/basic    3.283s
+
+//BenchmarkQuickSort-16，BenchmarkQuickSort表示所测试的测试函数名，16 表示有 16 个 CPU 线程参与了此次测试，默认是GOMAXPROCS的值。
+//1414204 ，说明函数中的循环执行了90848414次。
+//847.9 ns/op 说明每次循环的执行平均耗时是 847.9 纳秒，该值越小，说明代码性能越高。
+//1216 B/op  表示每次执行分配了多少内存（字节），该值越小，说明代码内存占用越小；
+//50 allocs/op 表示每次执行分配了多少次内存，该值越小，说明分配内存次数越少，意味着代码性能越高。
+
+//秒的换算:ms(毫秒),μs(微秒),ns(纳秒),ps(皮秒)
