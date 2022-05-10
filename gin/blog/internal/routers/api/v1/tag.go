863dc1e1a2fadd46f818cc5c02ca0f338fa77708
BinyGo <58040087@qq.com>
2022-05-10 22:21:29 +0800

    feat:gin-数据类型转换、分页处理、响应处理

--- gin/blog/internal/routers/api/v1/tag.go
+++ gin/blog/internal/routers/api/v1/tag.go
@@ -1,10 +1,9 @@
 package v1
 
 import (
-	"fmt"
-
 	"github.com/gin-gonic/gin"
-	"github.com/practic-go/gin/blog/global"
+	"github.com/practic-go/gin/blog/pkg/app"
+	"github.com/practic-go/gin/blog/pkg/errcode"
 )
 
 type Tag struct{}
@@ -15,9 +14,8 @@ func NewTag() Tag {
 
 func (t Tag) Get(c *gin.Context) {}
 func (t Tag) List(c *gin.Context) {
-	fmt.Println(global.ServerSetting)
-	fmt.Println(global.AppSetting)
-	fmt.Println(global.DatabaseSetting)
+	app.NewResponse(c).ToErrorResponse(errcode.ServerError)
+	return
 }
 func (t Tag) Create(c *gin.Context) {}
 func (t Tag) Update(c *gin.Context) {}
